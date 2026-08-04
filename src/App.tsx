7e74c82537386bc304539029dbde55e87f3b31ac
TimAaronSharp <timothyaaron.sharp@gmail.com>
2026-08-04 14:15:50 -0600

    REFACTORED: App.tsx - Wrapped <Header /> in a div with structural classNames.
    Header.tsx - Filled out component with header element.
    
    CREATED: index.css - Basic body css.

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,9 @@ import Header from "./components/Header";
 export default function App() {
   return (
     <>
-      <Header />
+      <div className="max-w-2xl mx-auto p-4 flex flex-col gap-4">
+        <Header />
+      </div>
     </>
   )
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,18 @@
 
 
 export default function Header() {
-    return (
-        <>
-            <h1>Header</h1>
-        </>
-    )
+  return (
+    <>
+      <header className="flex items-center justify-between">
+        <div className="flex flex-col gap-1">
+          <h1 className="text-3xl font-bold">Habit Tracker</h1>
+          <span className="text-zinc-400 text-sm">1 / 1 done today</span>
+        </div>
+        <div className="flex flex-col gap-1">
+          <span className="text-zinc-400 text-sm">Apr 6 - Apr 12</span>
+          <div className="flex items-center gap-3"></div>
+        </div>
+      </header>
+    </>
+  )
 }
