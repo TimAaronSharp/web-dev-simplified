8eff85ebceeb8f6a17caa428fa2b4d51637d1f74
TimAaronSharp <timothyaaron.sharp@gmail.com>
2026-08-04 17:01:28 -0600

    REFACTORED: Button.tsx - Added styling classes to button.
    Header.tsx - Added styling.

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -13,7 +13,7 @@ type ButtonProps = {
 export function Button({ children }: ButtonProps) {
   return (
     <>
-      <button>{children}</button>
+      <button className="bg-violet-600 hover:bg-violet-500 transition-colors rounded px-2 py-1 disabled:opacity-30 disabled:cursor-not-allowed">{children}</button>
     </>
   )
 }
