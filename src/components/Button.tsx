4be07cf68cc2532a406a20524cc9dbbc92ab1792
TimAaronSharp <timothyaaron.sharp@gmail.com>
2026-08-04 15:06:37 -0600

    REFACTORED: Button.tsx - Prop being passed is now a children ReactNode, receiving the children children of the <Button> components.

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,13 +1,19 @@
+import type { ReactNode } from "react";
 
+/*NOTE children is a property within every custom component in React
+you create. It is whatever you pass in between your component element.
+So when writing "<Button>Prev</Button>" in Header.tsx, "Prev" is the
+child/children property of this <Button> component. Children is a 
+"ReactNode" type.*/
 type ButtonProps = {
-  text: string;
+  children: ReactNode;
 }
 
 
-export function Button({ text }: ButtonProps) {
+export function Button({ children }: ButtonProps) {
   return (
     <>
-      <button>{text}</button>
+      <button>{children}</button>
     </>
   )
 }
