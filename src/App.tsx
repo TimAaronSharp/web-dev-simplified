7c441f83daecb4f5c4ef6513c442332e21166609
TimAaronSharp <timothyaaron.sharp@gmail.com>
2026-08-04 17:14:45 -0600

    CREATED: HabitForm.tsx - Form component for creating new habit. Imported into App.tsx.

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { HabitForm } from "./components/HabitForm.tsx";
 import { Header } from "./components/Header";
 
 
@@ -6,6 +7,7 @@ export default function App() {
     <>
       <div className="max-w-2xl mx-auto p-4 flex flex-col gap-4">
         <Header />
+        <HabitForm />
       </div>
     </>
   )
