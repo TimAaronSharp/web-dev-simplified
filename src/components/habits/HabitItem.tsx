d3d57a99a97fb51dcaeadf35f00e4ee37dfc7579
TimAaronSharp <timothyaaron.sharp@gmail.com>
2026-08-07 09:59:01 -0600

    INSTALLED: date-fns library for date handling.
    ADDED: HabitItem.tsx - Added div structure and buttons for days of the week. Still need to properly implement. Pushing to work on another device.

--- src/components/habits/HabitItem.tsx
+++ src/components/habits/HabitItem.tsx
@@ -1,4 +1,6 @@
+import { Button } from "../buttons/Button.tsx"
 import type { Habit } from "./HabitList.tsx"
+import { startOfWeek } from "date-fns"
 
 /*NOTE We pass down "habit" objects from HabitList to HabitItem as props.
 Props are passed down to a component as a single object. So the structure is
@@ -10,9 +12,26 @@ the habit.name property by only needing to type "name" within this component.
 property/object we are destructuring is of type "Habit", so that it will know
 that it contains both id: number and name: string properties. */
 export function HabitItem({ habit: { name } }: { habit: Habit }) {
+  const visibleDates = startOfWeek(new Date());
   return (
     <>
-      <h2>{name}</h2>
+      <div className="rounded-xl bg-zinc-800 p-4 flex flex-col gap-3">
+        <div className="flex items-center justify-between">
+          <div className="flex gap-3 items-center">
+            <span className="font-medium">{name}</span>
+            <span className="text-sm text-amber-400">🔥 3</span>
+          </div>
+          <Button>Delete</Button>
+        </div>
+        <div className="flex gap-1.5">
+          {visibleDates.map(date => (
+            <Button key={date.toISOString()}>
+              <span className="font-medium">Mon</span>
+              <span className="font-medium">2</span>
+            </Button>
+          ))}
+        </div>
+      </div>
     </>
   )
 
