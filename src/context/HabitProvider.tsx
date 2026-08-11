6655bba26c1e51ea12be9b741f39a071dee6a923
TimAaronSharp <timothyaaron.sharp@gmail.com>
2026-08-11 16:52:05 -0600

    REFACTORED: useLocalStorage.ts - Added a useEffect() to save to local storage. Also added a "dateReviver()" because apparently when dates are stringified and then parsed they remain a string and not a proper date, but this function is not working with the regex in the if(). Will investigate.

--- src/context/HabitProvider.tsx
+++ src/context/HabitProvider.tsx
@@ -1,6 +1,7 @@
 import { isSameDay } from "date-fns";
-import { useState, type ReactNode } from "react";
+import { type ReactNode } from "react";
 import { HabitContext, type Habit } from "./useHabits.ts";
+import { useLocalStorage } from "../hooks/useLocalStorage.ts";
 
 type HabitProviderProps = {
   children: ReactNode
@@ -8,7 +9,7 @@ type HabitProviderProps = {
 
 export function HabitProvider({ children }: HabitProviderProps) {
 
-  const [habits, setHabits] = useState<Habit[]>([]);
+  const [habits, setHabits] = useLocalStorage<Habit[]>("Habits", []);
 
   function addHabit(name: string) {
     setHabits(curr => ([...curr, { id: crypto.randomUUID(), name, completions: [] }]));
--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { parseISO } from "date-fns";
+import { useEffect, useState } from "react";
 
 
 export function useLocalStorage<T>(key: string, initialValue: T) {
@@ -7,12 +8,23 @@ export function useLocalStorage<T>(key: string, initialValue: T) {
       const item = localStorage.getItem(key)
 
       if (item == null) return initialValue;
-
+      // NOTE After getting dateReviver() to work it will be passed in as second argument in JSON.parse().
       return JSON.parse(item);
     } catch {
       return initialValue;
     }
   });
 
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(storedValue));
+  }, [storedValue, key]);
+
   return [storedValue, setStoredValue] as const
 }
+// NOTE Currently not working, regex (after "string") is causing an error. Investigate
+// function dateReviver(_key: string, value: unknown) {
+//   if (typeof value === "string" && /^\d{4}-\d{2}-\d{2}T/test(value)) {
+//     return parseISO(value);
+//   }
+//   return value;
+// }
